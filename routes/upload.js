2daf1403595f982e552de55b0b9805ade0e0b14e
DevThas <renart_63@hotmail.com>
2024-08-31 09:51:27 +0200

    commit-route

--- routes/upload.js
+++ routes/upload.js
@@ -4,22 +4,27 @@ const cloudinary = require("cloudinary").v2;
 const uniqid = require("uniqid");
 const fs = require("fs");
 
-
 router.post("/upload", async (req, res) => {
   const photoPath = `./tmp/${uniqid()}.jpg`;
-  const resultMove = await req.files.photoFromFront.mv(photoPath);
-  
- 
-  if (!resultMove) {
+
+  // const resultMove = await req.files.photoFromFront.mv(photoPath);
+  const galleryMove = await req.files.photoFromGallery.mv(photoPath);
+
+  // if (!resultMove) {
+  //   const resultCloudinary = await cloudinary.uploader.upload(photoPath);
+  //   res.json({ result: true, url: resultCloudinary.secure_url });
+  // } else {
+  //   res.json({ result: false, error: resultMove });
+  // }
+
+  if (!galleryMove) {
     const resultCloudinary = await cloudinary.uploader.upload(photoPath);
     res.json({ result: true, url: resultCloudinary.secure_url });
-    
   } else {
-    res.json({ result: false, error: resultMove });
+    res.json({ result: false, error: galleryMove });
   }
-  fs.unlinkSync(photoPath);
 
-  
+  fs.unlinkSync(photoPath);
 });
 
 module.exports = router;
